465ab7b847c3d2c440f5cfdb8391ab57e5e4e9f4
Byeongkuk Oh <byeongkukoh@gmail.com>
2024-10-07 14:11:16 +0900

    solve: [BOJ][B5] 2338. 긴자리 계산

---
+++ BaekJoon/Bronze/B5_긴자리 계산.py
@@ -0,0 +1,15 @@
+"""
+! 출처: https://www.acmicpc.net/problem/2338
+! 문제: 긴자리 계산
+! 난이도: 브론즈 5
+! 알고리즘 분류: 수학 / 사칙연산 / 임의 정밀도 / 큰 수 연산
+"""
+
+# * 풀이 1
+
+A = int(input())
+B = int(input())
+
+print(A + B)
+print(A - B)
+print(A * B)
