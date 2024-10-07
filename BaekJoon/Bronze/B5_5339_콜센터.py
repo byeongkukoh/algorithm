6f7d262fc7c7396fa9c6d74dfd842b7e6a93ecdb
Byeongkuk Oh <byeongkukoh@gmail.com>
2024-10-07 19:17:55 +0900

    solve: [BOJ][B5] 5339. 콜센터

---
+++ BaekJoon/Bronze/B5_5339_콜센터.py
@@ -0,0 +1,18 @@
+"""
+! 출처: https://www.acmicpc.net/problem/5339
+! 문제: 콜센터
+! 난이도: 브론즈 5
+! 알고리즘 분류: 구현
+"""
+
+# * 풀이 1
+print("     /~\\")
+print("    ( oo|")
+print("    _\\=/_")
+print("   /  _  \\")
+print("  //|/.\\|\\\\")
+print(" ||  \\ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
