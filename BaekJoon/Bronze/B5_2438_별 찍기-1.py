1176d23d1e0e0ba86bbf8a4cf4588018acb77f97
Byeongkuk Oh <byeongkukoh@gmail.com>
2024-10-07 14:03:37 +0900

    solve: [BOJ][B5] 2438. 별 찍기-1

---
+++ BaekJoon/Bronze/B5_2438_별 찍기-1.py
@@ -0,0 +1,18 @@
+"""
+! 출처: https://www.acmicpc.net/problem/2438
+! 문제: 별 찍기-1
+! 난이도: 브론즈 5
+! 알고리즘 분류: 구현
+"""
+
+# * 풀이 1
+
+# N = int(input())
+
+# for i in range(1, N+1):
+#     print("*" * i)
+    
+# * 풀이 2
+
+for i in range(1, int(input())+1):
+    print("*" * i)
