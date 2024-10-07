ab4646a2f5582b7ea20a9efaf97b2570bd6c05b5
Byeongkuk Oh <byeongkukoh@gmail.com>
2024-10-07 19:21:20 +0900

    solve: [BOJ][B5] 15740. A+B-9

---
+++ BaekJoon/Bronze/B5_15740_A+B-9.py
@@ -0,0 +1,12 @@
+"""
+! 출처: https://www.acmicpc.net/problem/15740
+! 문제: A + B - 9
+! 난이도: 브론즈 5
+! 알고리즘 분류: 수학 / 사칙연산 / 임의 정밀도 / 큰 수 연산
+"""
+
+# * 풀이 1
+
+A, B = map(int, input().split())
+
+print(A + B)
