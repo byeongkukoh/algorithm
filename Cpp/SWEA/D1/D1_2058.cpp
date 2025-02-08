dad42360a7f9552127d2a6ef44633eb427f899d2
Byeongkuk Oh <byeongkukoh@gmail.com>
2025-02-09 01:14:02 +0900

    improve : [C++][SWEA][D1] 2058. 자릿수 더하기

--- Cpp/SWEA/D1/D1_2058.cpp
+++ Cpp/SWEA/D1/D1_2058.cpp
@@ -1,19 +1,41 @@
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int main()
-{
-    char n[5];
-    scanf("%s", &n);
+// int main()
+// {
+//     char n[5];
+//     scanf("%s", &n);
 
-    int sum = 0;
+//     int sum = 0;
 
-    for (int i = 0; i < strlen(n); i++)
-    {
-        sum += int(n[i] - '0');
-    }
+//     for (int i = 0; i < strlen(n); i++)
+//     {
+//         sum += int(n[i] - '0');
+//     }
 
-    printf("%d", sum);
+//     printf("%d", sum);
 
-    return 0;
+//     return 0;
+// }
+
+// ====================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int number, sum = 0;
+  cin >> number;
+
+  while (number > 10) {
+    sum += number % 10;
+    number = number / 10;
+  }
+
+  sum += number;
+
+  cout << sum;
+
+  return 0;
 }
