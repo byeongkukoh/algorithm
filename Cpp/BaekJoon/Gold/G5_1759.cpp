5c74dc7754c68541534afa3d94b4090b591c4b6a
Byeongkuk Oh <byeongkukoh@gmail.com>
2025-03-11 19:53:22 +0900

    improve: [C++][BOJ][G5] 1759. 암호 만들기

--- Cpp/BaekJoon/Gold/G5_1759.cpp
+++ Cpp/BaekJoon/Gold/G5_1759.cpp
@@ -5,20 +5,24 @@
 using namespace std;
 
 int l, c;
+
 vector<char> vect;
 int visited[20];
 char path[20];
-int moreOne = 0;
-int moreTwo = 0;
 
-void func(int now) {
+bool isVowel(char ch) {
+  return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+}
+
+void func(int now, int vowels, int consonants) {
   if (now == l) {
-    if (moreOne < 1 || moreTwo < 2) return;
+    if (vowels >= 1 && consonants >= 2) {
+      for (int i = 0; i < l; i++) {
+        cout << path[i];
+      }
 
-    for (int i = 0; i < l; i++) {
-      cout << path[i];
+      cout << "\n";
     }
-    cout << "\n";
 
     return;
   }
@@ -27,23 +31,16 @@ void func(int now) {
     if (visited[i] == 1) continue;
     if (now > 0 && path[now - 1] > vect[i]) continue;
 
-    if (vect[i] == 'a' || vect[i] == 'e' || vect[i] == 'i' || vect[i] == 'o' ||
-        vect[i] == 'u') {
-      moreOne++;
+    if (isVowel(vect[i])) {
+      path[now] = vect[i];
+      visited[i] = 1;
+
+      func(now + 1, vowels + 1, consonants);
     } else {
-      moreTwo++;
-    }
+      path[now] = vect[i];
+      visited[i] = 1;
 
-    path[now] = vect[i];
-    visited[i] = 1;
-
-    func(now + 1);
-
-    if (vect[i] == 'a' || vect[i] == 'e' || vect[i] == 'i' || vect[i] == 'o' ||
-        vect[i] == 'u') {
-      moreOne--;
-    } else {
-      moreTwo--;
+      func(now + 1, vowels, consonants + 1);
     }
 
     path[now] = ' ';
@@ -63,7 +60,79 @@ int main() {
 
   sort(vect.begin(), vect.end());
 
-  func(0);
+  func(0, 0, 0);
 
   return 0;
 }
+
+// #include <algorithm>
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int l, c;
+// vector<char> vect;
+// int visited[20];
+// char path[20];
+// int moreOne = 0;
+// int moreTwo = 0;
+
+// void func(int now) {
+//   if (now == l) {
+//     if (moreOne < 1 || moreTwo < 2) return;
+
+//     for (int i = 0; i < l; i++) {
+//       cout << path[i];
+//     }
+//     cout << "\n";
+
+//     return;
+//   }
+
+//   for (int i = 0; i < vect.size(); i++) {
+//     if (visited[i] == 1) continue;
+//     if (now > 0 && path[now - 1] > vect[i]) continue;
+
+//     if (vect[i] == 'a' || vect[i] == 'e' || vect[i] == 'i' || vect[i] == 'o'
+//     ||
+//         vect[i] == 'u') {
+//       moreOne++;
+//     } else {
+//       moreTwo++;
+//     }
+
+//     path[now] = vect[i];
+//     visited[i] = 1;
+
+//     func(now + 1);
+
+//     if (vect[i] == 'a' || vect[i] == 'e' || vect[i] == 'i' || vect[i] == 'o'
+//     ||
+//         vect[i] == 'u') {
+//       moreOne--;
+//     } else {
+//       moreTwo--;
+//     }
+
+//     path[now] = ' ';
+//     visited[i] = 0;
+//   }
+// }
+
+// int main() {
+//   cin >> l >> c;
+
+//   for (int i = 0; i < c; i++) {
+//     char temp;
+//     cin >> temp;
+
+//     vect.push_back(temp);
+//   }
+
+//   sort(vect.begin(), vect.end());
+
+//   func(0);
+
+//   return 0;
+// }
