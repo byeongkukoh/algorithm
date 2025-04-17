414ccf73d387018e988627fde717ec6ee4f632af
Byeongkuk Oh <byeongkukoh@gmail.com>
2025-04-17 19:45:55 +0900

    solve: [C++][BOJ][G5] 11000. 강의실배정

--- Cpp/BaekJoon/Gold/G5_11000.cpp
+++ Cpp/BaekJoon/Gold/G5_11000.cpp
@@ -6,75 +6,133 @@
 using namespace std;
 
 struct Time {
-  long long start;
-  long long end;
+  int start;
+  int end;
+
+  bool operator<(Time right) const { return end > right.end; }
 };
 
 bool cmp(Time left, Time right) {
-  if (left.end < right.end) return true;
-  if (left.end > right.end) return false;
+  if (left.start != right.start) return left.start < right.start;
 
-  if (left.start < right.start) return true;
-  if (left.start > right.start) return false;
-
-  return false;
+  return left.end < right.end;
 }
 
 int main() {
   int n;
   cin >> n;
 
-  vector<Time> input;
+  vector<Time> lec;
 
   for (int i = 0; i < n; i++) {
-    long long start, end;
-    cin >> start >> end;
+    int sTime, eTime;
+    cin >> sTime >> eTime;
 
-    input.push_back({start, end});
+    lec.push_back({sTime, eTime});
   }
 
-  sort(input.begin(), input.end(), cmp);
+  sort(lec.begin(), lec.end(), cmp);
 
-  queue<Time> recture;
-  vector<Time> room;
+  priority_queue<Time> pque;
 
-  for (int i = 0; i < input.size(); i++) {
-    long long start = input[i].start;
-    long long end = input[i].end;
-
-    recture.push({start, end});
-  }
-
-  while (!recture.empty()) {
-    long long start = recture.front().start;
-    long long end = recture.front().end;
-
-    if (room.empty()) {
-      room.push_back({start, end});
-      recture.pop();
-
+  for (int i = 0; i < n; i++) {
+    if (pque.empty()) {
+      pque.push(lec[i]);
       continue;
     }
 
-    int room_cnt = room.size();
+    Time qtop = pque.top();
 
-    for (int i = 0; i < room_cnt; i++) {
-      if (room[i].end <= start) {
-        room[i].end = end;
-        recture.pop();
-        break;
-      }
-    }
-
-    if (start == recture.front().start && end == recture.front().end) {
-      room.push_back({start, end});
-      recture.pop();
+    if (qtop.end <= lec[i].start) {
+      pque.pop();
+      pque.push(lec[i]);
     } else {
-      continue;
+      pque.push(lec[i]);
     }
   }
 
-  cout << room.size();
+  cout << pque.size();
 
   return 0;
 }
+
+// #include <algorithm>
+// #include <iostream>
+// #include <queue>
+// #include <vector>
+
+// using namespace std;
+
+// struct Time {
+//   long long start;
+//   long long end;
+// };
+
+// bool cmp(Time left, Time right) {
+//   if (left.end < right.end) return true;
+//   if (left.end > right.end) return false;
+
+//   if (left.start < right.start) return true;
+//   if (left.start > right.start) return false;
+
+//   return false;
+// }
+
+// int main() {
+//   int n;
+//   cin >> n;
+
+//   vector<Time> input;
+
+//   for (int i = 0; i < n; i++) {
+//     long long start, end;
+//     cin >> start >> end;
+
+//     input.push_back({start, end});
+//   }
+
+//   sort(input.begin(), input.end(), cmp);
+
+//   queue<Time> recture;
+//   vector<Time> room;
+
+//   for (int i = 0; i < input.size(); i++) {
+//     long long start = input[i].start;
+//     long long end = input[i].end;
+
+//     recture.push({start, end});
+//   }
+
+//   while (!recture.empty()) {
+//     long long start = recture.front().start;
+//     long long end = recture.front().end;
+
+//     if (room.empty()) {
+//       room.push_back({start, end});
+//       recture.pop();
+
+//       continue;
+//     }
+
+//     int room_cnt = room.size();
+
+//     for (int i = 0; i < room_cnt; i++) {
+//       if (room[i].end <= start) {
+//         room[i].end = end;
+//         recture.pop();
+//         break;
+//       }
+//     }
+
+//     if (start == recture.front().start && end == recture.front().end) {
+//       room.push_back({start, end});
+//       recture.pop();
+//     } else {
+//       continue;
+//     }
+//   }
+
+//   cout << room.size();
+
+//   return 0;
+// }
