a3976a9464b649b9d237951edc89e9abfe4ac105
byeongkukoh <byeongkukoh@gmail.com>
2025-06-16 19:02:11 +0900

    solve: [Java][BOJ][B5] 7287. 등록

--- Java/BaekJoon/Bronze/B5_7287.java
+++ Java/BaekJoon/Bronze/B5_7287.java
@@ -1,6 +1,6 @@
-package BaekJoon;
+package BaekJoon.Bronze;
 
-public class Main {
+public class B5_7287 {
     public static void main(String[] args) {
         System.out.println("277");
         System.out.println("byeongkukoh");
