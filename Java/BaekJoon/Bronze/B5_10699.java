1dfbf1fdd4fae9602ff10fa62f6fb1608ab30f12
byeongkukoh <byeongkukoh@gmail.com>
2025-06-16 18:57:15 +0900

    solve: [Java][BOJ][B5] 10699. 오늘 날짜

--- Java/BaekJoon/Bronze/B5_10699.java
+++ Java/BaekJoon/Bronze/B5_10699.java
@@ -1,6 +1,6 @@
-package BaekJoon;
+package BaekJoon.Bronze;
 
-public class Main {
+public class B5_10699 {
     public static void main(String[] args) {
         System.out.println("2025-06-16");
     }
