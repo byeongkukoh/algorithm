99b3ad7f183d2d1235797d0a0a368ee35280a02b
byeongkukoh <byeongkukoh@gmail.com>
2025-06-28 19:40:30 +0900

    solve: [Java][BOJ][B5] 18108. 1998년생인 내가 태국에서는 2541년생?!

---
+++ Java/BaekJoon/Bronze/B5_18108.java
@@ -0,0 +1,15 @@
+package BaekJoon.Bronze;
+
+import java.util.Scanner;
+
+public class B5_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int inutYear = sc.nextInt();
+
+        System.out.println(inutYear - (2541 - 1998));
+
+        sc.close();
+    }
+}
