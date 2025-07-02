209f2258499f32be1fd44d459277e1c732485c9e
byeongkukoh <byeongkukoh@gmail.com>
2025-07-02 20:36:29 +0900

    solve: [Java][BOJ][B5] 30030. 스위트콘 가격 구하기

---
+++ Java/BaekJoon/Bronze/B5_30030.java
@@ -0,0 +1,15 @@
+package BaekJoon.Bronze;
+
+import java.util.Scanner;
+
+public class B5_30030 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int b = sc.nextInt();
+
+        int a = b / 110 * 100;
+
+        System.out.println(a);
+    }
+}
