ffb07877c0b162b3f50414a4b54f67ae6c90589c
byeongkukoh <byeongkukoh@gmail.com>
2025-07-30 23:11:58 +0900

    solve: [C++][BOJ][B3] 5997. 과제 안 내신 분..?

---
+++ C/BaekJoon/Bronze/B3_5597.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int arr[31] = {0};
+
+  for (int i = 0; i < 28; i++) {
+    int temp;
+    scanf("%d", &temp);
+
+    arr[temp] = 1;
+  }
+
+  for (int i = 1; i < 31; i++) {
+    if (arr[i] != 1) printf("%d\n", i);
+  }
+
+  return 0;
+}
