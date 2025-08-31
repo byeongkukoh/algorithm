30f892dae4f7db68fc1ccd70cf9b2b1e58d8bbbf
byeongkukoh <byeongkukoh@gmail.com>
2025-08-31 18:16:39 +0900

    solve: [Java][BOJ][B3] 2441. 별 찍기 - 4

---
+++ Java/BaekJoon/Bronze/B3_2441.java
@@ -0,0 +1,25 @@
+package BaekJoon.Bronze;
+
+import java.util.Scanner;
+
+public class B3_2441 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = n - i; j >= 1; j--) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        
+        sc.close();
+    }
+}
