977cfd8786758ca1b26b046db46dad2f1f00927c
byeongkukoh <byeongkukoh@gmail.com>
2025-12-06 10:07:20 +0900

    solve: [Java][BOJ][B5] 30007. 라면 공식

---
+++ Java/BaekJoon/Bronze/B5_30007.java
@@ -0,0 +1,21 @@
+package BaekJoon.Bronze;
+
+import java.util.Scanner;
+
+public class B5_30007 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int x = sc.nextInt();
+
+            System.out.println(a * (x - 1) + b);
+        }
+
+        sc.close();
+    }
+}
