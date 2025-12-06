6f17fa40664f335046d08f5561e966d3a4ad0ef3
byeongkukoh <byeongkukoh@gmail.com>
2025-12-06 12:15:58 +0900

    solve: [Python][BOJ][B5] 34691. 대전과학고등학교를 사랑하십니까?

---
+++ Python/BaekJoon/Bronze/B5_34691.py
@@ -0,0 +1,12 @@
+while (True):
+    name = input()
+
+    if (name == "end"):
+        break
+
+    if (name == "animal"):
+        print("Panthera tigris")
+    elif (name == "flower"):
+        print("Forsythia koreana")
+    elif (name == "tree"):
+        print("Pinus densiflora")
