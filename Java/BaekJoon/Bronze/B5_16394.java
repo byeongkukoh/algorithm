f2a2e1711bd8670abf71397165801dd584c20ca7
byeongkukoh <byeongkukoh@gmail.com>
2025-12-12 11:44:28 +0900

    solve: [Java][BOJ][B5] 16394. 홍익대학교

---
+++ Java/BaekJoon/Bronze/B5_16394.java
@@ -0,0 +1,16 @@
+package BaekJoon.Bronze;
+
+import java.util.Scanner;
+
+public class B5_16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(n - 1946);
+
+        sc.close();
+    }
+
+}
