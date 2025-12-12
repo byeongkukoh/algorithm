87141e02b796656f85afee1cd5e98580ed4b2805
byeongkukoh <byeongkukoh@gmail.com>
2025-12-12 11:52:16 +0900

    solve: [C++][BOJ][B5] 32775. 가희와 4시간의 벽 1

---
+++ Cpp/BaekJoon/Bronze/B5_32775.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  if (a <= b)
+    cout << "high speed rail";
+  else
+    cout << "flight";
+
+  return 0;
+}
