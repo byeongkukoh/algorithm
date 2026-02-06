8e69ac60bd09f3ea4c926e7ff60a652998340818
Oh Byeongkuk <byeongkukoh@gmail.com>
2026-02-06 12:52:35 +0900

    solve: [C++][LeetCode][Easy] 3379. Transformed Array

---
+++ Cpp/LeetCode/Easy/Easy_3379.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  vector<int> constructTransformedArray(vector<int>& nums) {
+    int n = nums.size();
+
+    vector<int> result(n, 0);
+    for (int i = 0; i < n; i++) {
+      if (nums[i] > 0) result[i] = nums[(i + nums[i]) % n];
+
+      if (nums[i] < 0) result[i] = nums[(i + nums[i] + (n * 100)) % n];
+
+      if (nums[i] == 0) result[i] = nums[i];
+    }
+
+    return result;
+  }
+};
